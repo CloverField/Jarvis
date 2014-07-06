afb9aa7d5c54a3e12b72da6376837dd900be5276
CloverField <blazelion3@gmail.com>
2014-07-05 22:09:28 -0400

    Now has a date time module.

---
+++ Jarvis/DateTme.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jarvis
+{
+    class DateTme
+    {
+        public string getCurrentDate()
+        {
+            DateTime dt = DateTime.Now;
+            return dt.ToShortDateString();
+        }
+
+        public string getCurrentTime()
+        {
+            DateTime dt = DateTime.Now;
+            return dt.ToShortTimeString();
+        }
+    }
+}
