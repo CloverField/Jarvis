7c4b2ac913ce2996d069ac966f935cae68184e94
CloverField <blazelion3@gmail.com>
2014-07-12 17:32:07 -0400

    Create classes for handling the creation of files and the generation of random numbers.

---
+++ Jarvis/Files.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Jarvis
+{
+    class Files
+    {
+        private bool canWriteHere(string path)
+        {
+            try
+            {
+                System.Security.AccessControl.DirectorySecurity ds = Directory.GetAccessControl(path);
+                return true;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return false;
+            }
+        }
+
+        public bool createFile(string path, string filename)
+        {
+            if (canWriteHere(path))
+            {
+
+                try
+                {
+                    System.IO.File.Create(path + "/" + filename);
+                    return true;
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Jarvis/RandomNumGen.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jarvis
+{
+    class RandomNumGen
+    {
+        private string generateRandomNum(string lower, string upper)
+        {
+            Random rand = new Random();
+            return rand.Next(Convert.ToInt32(lower), Convert.ToInt32(upper)).ToString();
+        }
+    }
+}
