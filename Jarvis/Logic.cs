e8230a531678f160b5957cd444d7cdb1cc720583
Jordan Harris <jmh6657@rit.edu>
2014-07-11 23:49:41 -0400

    Logic improvements
    Updated logic to respond to more test conditions.

--- Jarvis/Logic.cs
+++ Jarvis/Logic.cs
@@ -8,6 +8,24 @@ namespace Jarvis
 {
     class Logic
     {
+		public string checkInput(List<string> userInput)
+		{
+			SystemInfo Si = new SystemInfo();
+			Time t = new Time();
+			if (userTime (userInput)) 
+            {
+				return(Si.getUser () + " asked about the time the time is " + t.getCurrentTime ());
+			} 
+            else if (userDate (userInput)) 
+            {
+				return(Si.getUser () + " asked about the date the date is " + t.getCurrentDate ());
+			} 
+            else 
+            {
+				return("The user did not ask about the time or the date.");
+			}
+		}
+
 		public bool userTime(List<string> userInput)
         {
 			foreach (string s in userInput)
@@ -30,14 +48,14 @@ namespace Jarvis
             {
                 if(s.ToLower().Equals("date"))
                 {
-                    return true
-                }
+					return true;
+				}
                 else
                 {
-                    return false
+					return false;
                 }
             }
-            return false
+			return false;
     	}
     }
 }
--- Jarvis/Program.cs
+++ Jarvis/Program.cs
@@ -19,12 +19,9 @@ namespace Jarvis
             System.Console.WriteLine("The current date is " + t.getCurrentDate() + ".");
             System.Console.WriteLine("The currrent user is " + si.getUser() + ".");
             System.Console.WriteLine("The current OS version is " + si.getOS() + ".");
-            System.Console.WriteLine("Would you like know about the time? If you do please respond time.");
+			System.Console.WriteLine ("Would you like know about the time? If you do please respond time.");
             responce.Add(Console.ReadLine());
-            if (l.userTime(responce))
-            {
-                System.Console.WriteLine("The user asked about the time. The time is " + t.getCurrentTime() + ".");
-            }
+			System.Console.WriteLine (l.checkInput (responce));
             System.Console.ReadLine();
         }
     }
