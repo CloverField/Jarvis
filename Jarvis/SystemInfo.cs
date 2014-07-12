79dd3ded7f1b2c5b0f30c4558ab97b765ef0fe3b
CloverField <blazelion3@gmail.com>
2014-07-11 23:15:14 -0400

    Createsd System info class.

---
+++ Jarvis/SystemInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jarvis
+{
+    class SystemInfo
+    {
+        public string getUser()
+        {
+            return Environment.UserName;
+        }
+
+        public string getOS()
+        {
+            System.OperatingSystem osInfo = System.Environment.OSVersion;
+            return osInfo.ToString();
+        }
+    }
+}
