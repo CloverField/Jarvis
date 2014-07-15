0a41f751afd5ac12a05c59cb6799d5162043f9ba
Jordan Harris <jmh6657@rit.edu>
2014-07-14 21:55:40 -0400

    Crated a class for basic math functions.
    A class for simple math.

---
+++ Jarvis/Math.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Jarvis
+{
+	public class Math
+	{
+		public Math ()
+		{
+			public float add(float val1, float val2){
+				float result = val1 + val2
+				return result;
+			}
+
+			public float subtract(float val1, float val2){
+				float result = val1 + val2;
+				return result;
+			}
+
+			public float divide(float val1, float val2){
+				float result = val1/val2
+				return result;
+			}
+
+			public float multiply(float val1, float val2){
+				float result = val1 * val2 
+				return result;
+			}
+		}
+	}
+}
