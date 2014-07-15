29f59224172e5b0e68c65edbdafa17b31356cb00
CloverField <jmh6657@rit.edu>
2014-07-14 23:25:00 -0400

    Fixed scope.
    Math.cs will now correctly compile.

--- Jarvis/Math.cs
+++ Jarvis/Math.cs
@@ -4,31 +4,28 @@ namespace Jarvis
 {
 	public class Math
 	{
-		public Math ()
+		public float add(float val1, float val2)
 		{
-			public float add(float val1, float val2)
-			{
-				float result = val1 + val2
-				return result;
-			}
+			float result = val1 + val2;
+			return result;
+		}
 
-			public float subtract(float val1, float val2)
-			{
-				float result = val1 + val2;
-				return result;
-			}
+		public float subtract(float val1, float val2)
+		{
+			float result = val1 + val2;
+			return result;
+		}
 
-			public float divide(float val1, float val2)
-			{
-				float result = val1/val2
-				return result;
-			}
+		public float divide(float val1, float val2)
+		{
+			float result = val1/val2;
+			return result;
+		}
 
-			public float multiply(float val1, float val2)
-			{
-				float result = val1 * val2 
-				return result;
-			}
+		public float multiply(float val1, float val2)
+		{
+			float result = val1 * val2;
+			return result;
 		}
 	}
 }
